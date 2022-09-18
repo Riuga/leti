27ba0c53c89e6ba49f9afd770f278b28ef01715f
riuga <mosks@bk.ru>
2022-09-18 20:38:39 +0300

    init

---
+++ term 3/aadt/lab01/main.cpp
@@ -0,0 +1,16 @@
+/*
+  Lab01 by Moskvichev Sergei, group 1301
+  Variant 7
+  Проверка на содержание другого списка в списке, можно сделать целочисленного типа
+*/
+
+#include <iostream>
+#include "singly_linked_list.hpp"
+
+using namespace std;
+
+int main()
+{
+
+  return 0;
+}
