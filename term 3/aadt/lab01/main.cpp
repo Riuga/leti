a810c8dffe0a6d77a46c30a8f215f365331c6e2a
riuga <mosks@bk.ru>
2022-09-20 18:02:03 +0300

    aadt feat(lab01): tasks 1-11 done

--- term 3/aadt/lab01/main.cpp
+++ term 3/aadt/lab01/main.cpp
@@ -2,6 +2,18 @@
   Lab01 by Moskvichev Sergei, group 1301
   Variant 7
   Проверка на содержание другого списка в списке, можно сделать целочисленного типа
+
+  1. добавление в конец списка
+  2. добавление в начало списка
+  3. удаление последнего элемента
+  4. удаление первого элемента
+  5. добавление элемента по индексу (вставка перед элементом, который был ранее доступен по этому индексу)
+  6. получение элемента по индексу
+  7. удаление элемента по индексу
+  8. получение размера списка
+  9. удаление всех элементов списка
+  10. замена элемента по индексу на передаваемый элемент
+  11. проверка на пустоту списка
 */
 
 #include <iostream>
@@ -9,8 +21,109 @@
 
 using namespace std;
 
+void show_menu()
+{
+  cout << "Type 1 to show menu" << endl;
+  cout << "Type 2 to push an element back" << endl;
+  cout << "Type 3 to push an element front" << endl;
+  cout << "Type 4 to delete the last element" << endl;
+  cout << "Type 5 to delete the first element" << endl;
+  cout << "Type 6 to add an element by index" << endl;
+  cout << "Type 7 to get an element by index" << endl;
+  cout << "Type 8 to remove an element by index" << endl;
+  cout << "Type 9 to get size of the list" << endl;
+  cout << "Type 10 to clear the list" << endl;
+  cout << "Type 11 to change an element" << endl;
+  cout << "Type 12 to check the list on emptiness" << endl;
+  cout << "Type 13 to print a list" << endl;
+  cout << "Type 0 to exit" << endl;
+}
+
 int main()
 {
+  int i;
+  int index;
+  int val;
+  List new_list;
+
+  show_menu();
+  while (i != 0)
+  {
+    cin >> i;
+
+    switch (i)
+    {
+    case 1:
+      show_menu();
+      break;
+    case 2:
+      cout << "Enter a value: " << endl;
+      cin >> val;
+      new_list.push_back(val);
+      break;
+    case 3:
+      cout << "Enter a value: " << endl;
+      cin >> val;
+      new_list.push_front(val);
+      break;
+    case 4:
+      cout << "Element was removed" << endl;
+      new_list.pop_back();
+      break;
+    case 5:
+      cout << "Element was removed" << endl;
+      new_list.pop_front();
+      break;
+    case 6:
+      cout << "Enter a value: " << endl;
+      cin >> val;
+      cout << "Enter an index: " << endl;
+      cin >> index;
+      new_list.insert_by_index(index, val);
+      break;
+    case 7:
+      cout << "Enter an index: " << endl;
+      cin >> index;
+      cout << "An element: " << new_list.get_value(index) << endl;
+      break;
+    case 8:
+      cout << "Enter an index: " << endl;
+      cin >> index;
+      new_list.remove_element(index);
+      break;
+    case 9:
+      cout << "Size: " << new_list.get_size() << endl;
+      break;
+    case 10:
+      new_list.clear();
+      cout << "List was cleared" << endl;
+      break;
+    case 11:
+      cout << "Enter a value: " << endl;
+      cin >> val;
+      cout << "Enter an index: " << endl;
+      cin >> index;
+      new_list.change_value(index, val);
+      break;
+    case 12:
+      if (new_list.is_empty())
+      {
+        cout << "List is empty" << endl;
+      }
+      else
+      {
+        cout << "List is not empty" << endl;
+      }
+      break;
+
+    case 13:
+      new_list.print_list();
+      break;
+
+    default:
+      break;
+    }
+  }
 
   return 0;
 }
--- term 3/aadt/lab01/singly_linked_list.hpp
+++ term 3/aadt/lab01/singly_linked_list.hpp
@@ -1,15 +1,179 @@
 /*
   TODO:
-  1. добавление в конец списка
-  2. добавление в начало списка
-  3. удаление последнего элемента
-  4. удаление первого элемента
-  5. добавление элемента по индексу (вставка перед элементом, который был ранее доступен по этому индексу)
-  6. получение элемента по индексу
-  7. удаление элемента по индексу
-  8. получение размера списка
-  9. удаление всех элементов списка
-  10. замена элемента по индексу на передаваемый элемент
-  11. проверка на пустоту списка
   16. Проверка на содержание другого списка в списке, можно сделать целочисленного типа
 */
+#include <iostream>
+
+using namespace std;
+
+struct Node
+{
+  int value;
+  Node *next_element;
+
+  Node(int val) : value(val), next_element(nullptr){};
+};
+
+struct List
+{
+  Node *first_element;
+  Node *last_element;
+
+  List() : first_element(nullptr), last_element(nullptr){};
+
+  bool is_empty()
+  {
+    return first_element == nullptr;
+  }
+
+  void push_front(int val)
+  {
+    Node *new_element = new Node(val);
+    if (is_empty())
+    {
+      first_element = new_element;
+      last_element = new_element;
+
+      return;
+    }
+
+    new_element->next_element = first_element;
+    first_element = new_element;
+  }
+
+  void push_back(int val)
+  {
+    Node *new_element = new Node(val);
+
+    if (is_empty())
+    {
+      first_element = new_element;
+      last_element = new_element;
+
+      return;
+    }
+    last_element->next_element = new_element;
+    last_element = new_element;
+  }
+
+  void pop_front()
+  {
+    if (is_empty())
+    {
+      return;
+    }
+    Node *temp = first_element;
+    first_element = temp->next_element;
+    delete temp;
+  }
+
+  void pop_back()
+  {
+    if (is_empty())
+    {
+      return;
+    }
+    if (first_element == last_element)
+    {
+      pop_front();
+    }
+    Node *temp = first_element;
+    while (temp->next_element != last_element)
+    {
+      temp = temp->next_element;
+    }
+    temp->next_element = nullptr;
+    delete last_element;
+    last_element = temp;
+  }
+
+  unsigned int get_size()
+  {
+    unsigned int size = 0;
+    Node *temp = first_element;
+    while (temp)
+    {
+      temp = temp->next_element;
+      size++;
+    }
+    return size;
+  }
+
+  Node *get_element(unsigned int index)
+  {
+    if (is_empty() || index >= get_size())
+    {
+      return nullptr;
+    }
+
+    Node *element = first_element;
+    unsigned int i = 0;
+    while (i < index)
+    {
+      element = element->next_element;
+      i++;
+    }
+
+    return element;
+  }
+
+  int get_value(unsigned int index)
+  {
+    return get_element(index)->value;
+  }
+
+  void remove_element(unsigned int index)
+  {
+    if (is_empty() || index >= get_size())
+    {
+      return;
+    }
+    Node *del_element = get_element(index);
+    Node *temp = first_element;
+    unsigned int i = 0;
+    while (i < index - 1)
+    {
+      temp = temp->next_element;
+      i++;
+    }
+
+    temp->next_element = del_element->next_element;
+    delete del_element;
+  }
+
+  void clear()
+  {
+    while (!is_empty())
+    {
+      pop_front();
+    }
+  }
+
+  void change_value(unsigned int index, int val)
+  {
+    Node *change = get_element(index);
+    change->value = val;
+  }
+
+  void insert_by_index(unsigned int index, int val)
+  {
+    Node *new_element = new Node(val);
+    new_element->next_element = get_element(index);
+    get_element(index - 1)->next_element = new_element;
+  }
+
+  void print_list()
+  {
+    if (is_empty())
+    {
+      return;
+    }
+    Node *temp = first_element;
+    while (temp)
+    {
+      cout << temp->value << " ";
+      temp = temp->next_element;
+    }
+    cout << endl;
+  }
+};
