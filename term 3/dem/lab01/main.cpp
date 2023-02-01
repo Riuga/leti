7c92a6cacb6a03b00861bfa1312c21a402f3e358
riuga <mosks@bk.ru>
2023-02-01 03:18:52 +0300

    feat dem (lab01): done

--- term 3/dem/lab01/main.cpp
+++ term 3/dem/lab01/main.cpp
@@ -1,22 +1,81 @@
-// Variant 11
-
 #include <iostream>
-#include <string>
+#include <cmath>
 #include <cstdint>
-#include "transformations.hpp"
+#include <string>
 
 using namespace std;
 
+const uint8_t BYTE_SIZE = 8;
+
+union type
+{
+  unsigned long long ll;
+  long double d;
+};
+
+template <typename T>
+void to_binary(const T &num)
+{
+  const size_t bits = BYTE_SIZE * sizeof(num);
+  size_t mask = static_cast<size_t>(pow(2, bits)) - 1;
+  size_t num_bits = *(size_t *)(&num) & mask;
+  string output;
+
+  output.resize(bits);
+
+  for (size_t n = 0; n < bits; n++)
+  {
+    output[bits - n - 1] = (num_bits & 1 ? '1' : '0');
+    num_bits >>= 1;
+  }
+
+  cout << output << endl;
+}
+
+unsigned long long reverse_long_double(long double ldnum, int count, int num)
+{
+  unsigned long long shift = (unsigned long long)num - (unsigned long long)count + 1;
+  type in_long_double;
+  in_long_double.d = ldnum;
+  unsigned long long mask = ((1 << (unsigned long long)count) - 1) << shift;
+  unsigned long long ext = (in_long_double.ll & mask) >> shift;
+  unsigned long long rev_ext = 0;
+  for (int i = 0; i < count; ++i)
+  {
+    rev_ext <<= 1;
+    rev_ext |= ext & 1;
+    ext >>= 1;
+  }
+  rev_ext <<= shift;
+
+  return (in_long_double.ll & ~mask) | rev_ext;
+}
+
+unsigned int reverse_char(unsigned int num, int c, int n)
+{
+  int shift = n - c + 1;
+  unsigned int mask = ((1 << c) - 1) << shift;
+  unsigned int ext = (num & mask) >> shift;
+  unsigned int rev_ext = 0;
+  int i;
+  for (i = 0; i < c; ++i)
+  {
+    rev_ext <<= 1;
+    rev_ext |= ext & 1;
+    ext >>= 1;
+  }
+  rev_ext <<= shift;
+
+  return (num & ~mask) | rev_ext;
+}
+
 int main()
 {
-  int i;
-  uint8_t index;
-  uint8_t amount;
-  char conv_num;
   string num;
-  char yes;
+  char chnum;
   long double ldnum;
-  size_t binary_num;
+  int i;
+  char yes;
 
   cout << "Type 1 to transform char to binary" << endl;
   cout << "Type 2 to transform long double to binary" << endl;
@@ -35,24 +94,25 @@ int main()
       cin >> num;
       cout << endl;
 
-      conv_num = (char)atoi(num.c_str());
-      to_binary<char>(conv_num);
+      chnum = (char)atoi(num.c_str());
+      to_binary(chnum);
 
       cout << "Do you want to mirror some numbers? y/N" << endl;
-
       cin >> yes;
 
       if (yes == 'y')
       {
-        cout << "Enter an index: ";
-        cin >> index;
-
+        int amount;
+        int begin;
         cout << "Enter an amount of numbers to shuffle: ";
-        cin >> amount;
+        scanf("%d", &amount);
 
-        mirror_range<char>(&conv_num, index, amount);
-        to_binary(conv_num);
-        cout << (int)conv_num << endl;
+        cout << "Enter an index to start with: ";
+        scanf("%d", &begin);
+
+        char reversed = reverse_char(chnum, amount, begin);
+        printf("%u\n", reversed);
+        to_binary(reversed);
       }
 
       break;
@@ -62,28 +122,32 @@ int main()
       cin >> ldnum;
       cout << endl;
 
-      to_binary<long double>(ldnum);
+      to_binary(ldnum);
 
       cout << "Do you want to mirror some numbers? y/N" << endl;
       cin >> yes;
 
       if (yes == 'y')
       {
-        cout << "Enter an index: ";
-        cin >> index;
+        int amount;
+        int begin;
+        long double result;
 
-        cout << "Enter an amount of numbers to shuffle: ";
+        cout << "Enter amount of numbers to shuffle: ";
         cin >> amount;
+        cout << "Enter an index to start with: ";
+        cin >> begin;
 
-        mirror_range<__int128>(&ldnum, index, amount);
-        cout << ldnum << endl;
+        result = reverse_long_double(ldnum, amount, begin);
+        to_binary(result);
+        cout << result << endl;
       }
       break;
 
     default:
       break;
     }
-  }
 
-  return 0;
+    return 0;
+  }
 }
